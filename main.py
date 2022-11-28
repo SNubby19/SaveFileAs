ec350f125b7cfc154443a312c77255a2bba1eb37
Sujash Nayak <sujash.nayak@gmail.com>
2022-11-28 00:17:28 -0500

    changes are the same as mentioned prev commit

--- main.py
+++ main.py
@@ -3,15 +3,17 @@ import os
 
 dir_scan = list(os.scandir())
 
-ftc = dir_scan[0].path
+ftc = ""
 
 for entry in dir_scan:
+    ftc = entry.path
+
     if (os.path.getmtime(entry.path) <= os.path.getmtime(ftc)) and (entry.path[-4:] == "docx"):
         ftc = entry.path
 
 ftc = os.path.basename(ftc)
 sfa = ftc[:-4] + "pdf"
 
-opath = "C:/Users/YOURUSERNAME/Documents/PDF Saves/" + sfa
+opath = "C:\\Users\\USER\\Documents\\PDF Saves\\" + sfa
 
 d2p.convert(ftc, opath)
